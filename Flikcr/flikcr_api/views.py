a50a10da01bea9d875b5c8a7b157a1106b86cc50
Mahmoudreza-Izadiyar <m.izadiyar@gmail.com>
2021-01-23 00:10:37 +1100

    Build structure

--- Flikcr/flikcr_api/views.py
+++ Flikcr/flikcr_api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+    context = {
+        "name": "mahmoudreza",
+    }
+    return render(request, 'flikcr_api/index.html', context)
